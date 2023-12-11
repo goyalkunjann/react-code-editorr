93738dfffcd038663bf075bd10ae380e262e3df9
goyalkunjann <kunjangoyal2002@gmail.com>
2023-12-11 09:25:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# CodeRush - Compile and Execute code in languages
+
+
+<img src="https://github.com/goyalkunjann/react-code-editorr/blob/main/demo.png" />
+
+## Features: 
+- Compile and execute code in  programming languages.
+- Switch themes from a list of available themes.
+
+## Installations and setup
+
+- git clone `https://github.com/goyalkunjann/react-code-editorr.git`
+- `npm install`
+- A sample `.env.sample` file is given, Register on <a href="https://rapidapi.com/judge0-official/api/judge0-ce/pricing" target="__blank">RapidAPI</a> and get your API keys.
+- Create a `.env` file.
+- Add the API Keys in the `.env` file
+- `npm start` to run the project.
+
+
+
+
+
+
---
+++ src/constants/languageOptions.js
@@ -0,0 +1,106 @@
+export const languageOptions = [{
+        id: 63,
+        name: "JavaScript (Node.js 12.14.0)",
+        label: "JavaScript (Node.js 12.14.0)",
+        value: "javascript",
+    },
+    {
+        id: 48,
+        name: "C (GCC 7.4.0)",
+        label: "C (GCC 7.4.0)",
+        value: "c",
+    },
+    {
+        id: 52,
+        name: "C++ (GCC 7.4.0)",
+        label: "C++ (GCC 7.4.0)",
+        value: "cpp",
+    },
+    {
+        id: 49,
+        name: "C (GCC 8.3.0)",
+        label: "C (GCC 8.3.0)",
+        value: "c",
+    },
+    {
+        id: 53,
+        name: "C++ (GCC 8.3.0)",
+        label: "C++ (GCC 8.3.0)",
+        value: "cpp",
+    },
+    {
+        id: 50,
+        name: "C (GCC 9.2.0)",
+        label: "C (GCC 9.2.0)",
+        value: "c",
+    },
+    {
+        id: 54,
+        name: "C++ (GCC 9.2.0)",
+        label: "C++ (GCC 9.2.0)",
+        value: "cpp",
+    },
+
+    {
+        id: 51,
+        name: "C# (Mono 6.6.0.161)",
+        label: "C# (Mono 6.6.0.161)",
+        value: "csharp",
+    },
+    {
+        id: 60,
+        name: "Go (1.13.5)",
+        label: "Go (1.13.5)",
+        value: "go",
+    },
+    {
+        id: 62,
+        name: "Java (OpenJDK 13.0.1)",
+        label: "Java (OpenJDK 13.0.1)",
+        value: "java",
+    },
+
+    {
+        id: 78,
+        name: "Kotlin (1.3.70)",
+        label: "Kotlin (1.3.70)",
+        value: "kotlin",
+    },
+    {
+        id: 43,
+        label: "Plain Text",
+        name: "Plain Text",
+        value: "text",
+    },
+    {
+        id: 70,
+        name: "Python (2.7.17)",
+        label: "Python (2.7.17)",
+        value: "python",
+    },
+    {
+        id: 71,
+        name: "Python (3.8.1)",
+        label: "Python (3.8.1)",
+        value: "python",
+    },
+    {
+        id: 80,
+        name: "R (4.0.0)",
+        label: "R (4.0.0)",
+        value: "r",
+    },
+
+    {
+        id: 82,
+        name: "SQL (SQLite 3.27.2)",
+        label: "SQL (SQLite 3.27.2)",
+        value: "sql",
+    },
+    {
+        id: 84,
+        name: "Visual Basic.Net (vbnc 0.0.0.5943)",
+        label: "Visual Basic.Net (vbnc 0.0.0.5943)",
+        value: "vbnet",
+    },
+];
